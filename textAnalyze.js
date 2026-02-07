9d5c38f2e444a0128b2a596a3c80339527e8b696
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-02-07 23:13:45 +0600

    solved text analyze case

--- textAnalyze.js
+++ textAnalyze.js
@@ -2,6 +2,9 @@ function analyzeText(str) {
   if (typeof str !== "string") {
     return "Invalid";
   }
+  if (str.length < 1) {
+    return "Invalid";
+  }
   let splitStr = str.split(" ");
   let longestWord = splitStr[0];
   for (let word of splitStr) {
@@ -21,5 +24,5 @@ function analyzeText(str) {
     token: letters,
   };
 }
-let output = analyzeText("Keep coding keep shining");
+let output = analyzeText("I am a little honest person");
 console.log(output);
